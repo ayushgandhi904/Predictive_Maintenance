b9f71e759fd7ca973271d66100da5ac3cc18cd3e
Your Name <ayushgandhi904@gmail.com>
2023-09-24 11:19:13 +0530

    logger & exception added

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging 
+import os 
+from datetime import datetime 
+
+#Definining the log Format
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),'logs',LOG_FILE) 
+os.makedirs(logs_path,exist_ok=True) 
+
+#Definining the log path
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE) 
+
+#Defining the log level type: --> INFO
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
